e05d56fa9ca808dadc6dcdfe19147e41a7100109
unknown <Maxel26>
2023-06-28 21:07:00 -0500

    [+] index.route : Define ruta/Users

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import express, {Request, Response} from "express" ;
+import routeMain from "./routes";
 
 
 
 const app = express();
 
 // Routing: Enrutamiento 
-// http://localhost:3000/
+// http://localhost:3000/ 
 app.get('/', (req : Request, res : Response) => {
     const message : string = 'Bienvenido a la API de MudarteCol'
     
@@ -13,7 +14,7 @@ app.get('/', (req : Request, res : Response) => {
     res.send(`<h1> ${message}</h1>`); // Mensaje al navegador 
 });
 
-// http:localhost: 3000/home
+// http:localhost: 3000/home (Endpoint)
 app.get('/home', (req : Request, res : Response) => {
     const namepage : string = 'Home'
 
@@ -21,7 +22,11 @@ app.get('/home', (req : Request, res : Response) => {
     res.send(`<h1> ${namepage}</h1>`);
 })
 
+// Middleware
+app.use('/', routeMain );
 
 app.listen(3000, () => {
     console.log('Servidor lanzado en el puerto 3000'); 
 });
+
+
