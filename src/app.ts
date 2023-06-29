c9a80972b3b685867982b379e56a7ae6f127d4bf
unknown <Maxel26>
2023-06-28 20:30:23 -0500

    [+] app.ts: define 2 endpoints, / , /Home

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,25 @@
 import express from "express" ;
 
 const app = express();
+
+// Routing: Enrutamiento 
+// http://localhost:3000/
+app.get('/', (req,res) => {
+    const message = 'Bienvenido a la API de MudarteCol'
+    
+    console.log(message); // Mensaje a la consola  
+    res.send(`<h1> ${message}</h1>`); // Mensaje al navegador 
+});
+
+// http:localhost: 3000/home
+app.get('/home', (req, res) => {
+    const namepage = 'Home'
+
+    console.log(namepage);  
+    res.send(`<h1> ${namepage}</h1>`);
+})
+
+
 app.listen(3000, () => {
     console.log('Servidor lanzado en el puerto 3000'); 
 });
