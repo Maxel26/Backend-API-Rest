2e24a5e29cbf56730a6baca7af530f8f9bf8cfe4
unknown <Maxel26>
2023-06-28 20:38:31 -0500

    [+] app.ts: Agregamos tipos a variables de Express

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,21 @@
-import express from "express" ;
+import express, {Request, Response} from "express" ;
+
+
 
 const app = express();
 
 // Routing: Enrutamiento 
 // http://localhost:3000/
-app.get('/', (req,res) => {
-    const message = 'Bienvenido a la API de MudarteCol'
+app.get('/', (req : Request, res : Response) => {
+    const message : string = 'Bienvenido a la API de MudarteCol'
     
     console.log(message); // Mensaje a la consola  
     res.send(`<h1> ${message}</h1>`); // Mensaje al navegador 
 });
 
 // http:localhost: 3000/home
-app.get('/home', (req, res) => {
-    const namepage = 'Home'
+app.get('/home', (req : Request, res : Response) => {
+    const namepage : string = 'Home'
 
     console.log(namepage);  
     res.send(`<h1> ${namepage}</h1>`);
