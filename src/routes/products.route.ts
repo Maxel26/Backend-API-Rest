e79bc38dfd72ce55b3dae368b05977defdabe47a
unknown <Maxel26>
2023-06-29 21:48:31 -0500

    [+] Vinculamos rutas al controlador

--- src/routes/products.route.ts
+++ src/routes/products.route.ts
@@ -1,10 +1,15 @@
 // Archivo central de enrutamiento 
 import {Router, Request , Response} from 'express'
-import {getProducts} from '../controllers/product.controller'
+import {createProduct, deleteProduct, getProduct, getProducts, updateProduct} from '../controllers/product.controller'
 
 const router = Router();
 
 router.get('/', getProducts)
+router.get('/', getProduct)
+router.post('/', createProduct);
+router.put('/', updateProduct)
+router.delete('/', deleteProduct)
+
 
 
 export default router ;
