eb41fbd6ed2032722580f60798aea3198f47ccbb
unknown <Maxel26>
2023-06-30 21:51:26 -0500

    [+] mongo.confing : Se crea variable de entorno para URI a MongoDB [+] env.example

--- src/config/mongo.config.ts
+++ src/config/mongo.config.ts
@@ -3,7 +3,7 @@ import { connect } from "mongoose";
 // TODO: Resolver conexion de Mongo con Mongoose
 // Configuracion para conexion con MongoDB
 async function dbconnect() : Promise<void> {
-    const DB_URI = 'mongodb://127.0.0.1:27017/store';
+    const DB_URI = `${process.env.DB_URI}`;
     await connect(DB_URI);
 }
 
