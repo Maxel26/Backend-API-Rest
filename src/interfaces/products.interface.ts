7908a7169dd55d54e3f2ca7f9d2c2f81f21c382a
unknown <Maxel26>
2023-06-30 20:28:29 -0500

    [+] products.faces : Define estructura de la interfaz para productos

---
+++ src/interfaces/products.interface.ts
@@ -0,0 +1,7 @@
+// Interface: Solamente se modela su estructura de datos y sus tipos 
+// NOTA : las interfaces solo modelan y definen sus datos y no tienen metodos, en cambio las clases si 
+export interface Product {
+    name : string;
+    price : number;
+    description : string;
+}
