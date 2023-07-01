be62fa8606df66340932e08bae9274e825b3668a
unknown <Maxel26>
2023-06-30 20:30:12 -0500

    [+] product.model: Define estructura de la entidad (Modelo BD)

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -2,23 +2,28 @@ import { Request, Response } from "express"
 
 
 async function getProducts(req: Request, res: Response){
-    console.log(`Obtiene todo los productos`);  
+    console.log(`Obtiene todo los productos`);
+    res.send(`Obtiene todo los productos`);
 };
 
 async function getProduct(req: Request, res: Response){
     console.log(`obtiene un producto por ID`);  
+    res.send(`obtiene un producto por ID`);
 };
 
 async function createProduct(req: Request, res: Response){
     console.log(`Crea un producto`);  
+    res.send(`Crea un producto`);
 };
 
 async function updateProduct(req: Request, res: Response){
-    console.log(`Actualiza un producto por ID`);  
+    console.log(`Actualiza un producto por ID`); 
+    res.send(`Actualiza un producto por ID`);
 };
 
 async function deleteProduct(req: Request, res: Response){
     console.log(`Elimina un producto por ID`);  
+    res.send(`Elimina un producto por ID`);
 };
 
 export {
---
+++ src/models/products.model.ts
@@ -0,0 +1,30 @@
+// La estructura que definimos se vera reflejada en nuestra base de datos
+import { Schema, model } from "mongoose";
+
+
+// Define estructura de datos en la base de datos 
+const productSchema =  new Schema(
+//    Objeto principal que definira atributos del modelo
+    {
+        name: {
+            type : String,
+            required : true
+        },
+        price : {
+            type : Number,
+            required : false
+        },
+        description: {
+            type : String
+        }
+    },
+//  Definira configuraciones que se pueden aplicar en Mongoose para ese objeto
+    {
+        timestamps : true
+    }   
+);
+
+// Define el modelo apartir de la estructura requerida por Mongoose 
+const productModel = model( 'Products', productSchema );
+
+export default productModel ;
